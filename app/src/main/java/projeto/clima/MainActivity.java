63cbc1fca0e49e58b39d35cba3d92a1dd88dc54d
Electtro1 <122417639+Electtro1@users.noreply.github.com>
2024-04-10 22:28:56 -0300

    Primeira parte

--- app/src/main/java/projeto/clima/MainActivity.java
+++ app/src/main/java/projeto/clima/MainActivity.java
@@ -1,24 +1,56 @@
-package projeto.clima;
+package org.example;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import java.io.IOException;
+import java.time.LocalDate;
 
-import android.os.Bundle;
+public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+    public static void main(String[] args) {
+        String url = "https://www.climatempo.com.br/previsao-do-tempo/cidade/558/saopaulo-sp";
+        try {
+            Document doc = Jsoup.connect(url).get();
+            Element tempMinElement = doc.getElementById("min-temp-1");
+            String tempMin = "";
+            if (tempMinElement != null) {
+                tempMin = tempMinElement.text();
+            }
 
-public class MainActivity extends AppCompatActivity {
+            Element tempMaxElement = doc.getElementById("max-temp-1");
+            String tempMax = "";
+            if (tempMaxElement != null) {
+                tempMax = tempMaxElement.text();
+            }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+            Element chuvaInfo = doc.select("._margin-l-5").get(2);
+            String[] chuvaData = chuvaInfo.text().split(" ");
+            String chuvaMm = chuvaData[0];
+            String chuvaPorcento = chuvaData[2].replace("%", "");
+            Element qualidadeArElement = doc.select(".value").first();
+            String qualidadeAr = "";
+            if (qualidadeArElement != null) {
+                qualidadeAr = qualidadeArElement.text().trim();
+            }
+
+            LocalDate date = LocalDate.now();
+            int dia = date.getDayOfMonth();
+            int mes = date.getMonthValue();
+            int ano = date.getYear();
+
+            System.out.println("Previsão de Hoje " + dia + "/" + mes + "/" + ano + "  em São Paulo - SP");
+            System.out.println("Temperatura Máxima: " + tempMax);
+            System.out.println("Temperatura Mínima: " + tempMin);
+            System.out.println("Chuva: " + chuvaMm + " - " + chuvaPorcento + "%");
+            if (Integer.parseInt(chuvaPorcento) > 60) {
+                System.out.println("Grandes Possibilidades de chuva!");
+            }
+            System.out.println("Qualidade do ar: " + qualidadeAr);
+        } catch (IOException e) {
+            logger.error("Erro ao conectar ao URL: " + url, e);
+        }
     }
 }
